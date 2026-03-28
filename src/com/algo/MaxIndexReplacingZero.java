8a21d945aa45db7aba47bba59ce539cc4532cab7
hossain <opu.2k7.cse.kuet@gmail.com>
2026-03-28 23:04:47 +0200

    Maximum subarray containing 1 by replacing single 0 in a biary array

---
+++ src/com/algo/MaxIndexReplacingZero.java
@@ -0,0 +1,39 @@
+package com.algo;
+
+public class MaxIndexReplacingZero {
+
+
+
+    private static void findIndex(int[] A) {
+
+        int maxIndex = -1;
+        int maxCount = Integer.MIN_VALUE;
+
+        int previousIndex = -1;
+        int currentCount = 0;
+
+        for (int i=0; i<A.length; i++) {
+
+            if (A[i] >0) {
+                currentCount ++;
+            } else {
+                currentCount = i-previousIndex;
+                previousIndex = i;
+            }
+
+            if (currentCount > maxCount) {
+                maxCount = currentCount;
+                maxIndex = previousIndex;
+            }
+        }
+        System.out.println("==Index:"+maxIndex+" Count:"+maxCount);
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] A = { 0, 0, 1, 0, 1, 1, 1, 0, 1, 1 };
+
+        findIndex(A);
+    }
+}
